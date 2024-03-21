8615aca43575b61ff2cad98e7368c56659d85a05
Adnan <adnan_silva54@hotmail.com>
2024-03-21 03:00:44 -0300

    fix gitignore

--- FuelWise.V2/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace FuelWise.V2;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
